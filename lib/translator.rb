3d447a7dd866b6d8ea56db7b3c8d84b4cd558180
Arthur Hwang <arthur.hwang@yale.edu>
2020-05-24 01:34:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,29 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+# Method loads emoticons.yml file and returns HoH 
+def load_library(library)
+  emoticon1 = YAML.load_file(library)
+  final_results = emoticon1.each_with_object({}) do |(k,v), a|
+    a[k.to_sym] = {:english => v[0], :japanese => v[1]}
+  end
+  final_results
+  binding.pry
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+# Method takes Western emoticon and returns Japanese emoticon
+def get_japanese_emoticon(library, emoticon)
+  hash <- load_library(library)
+  result <- hash.select do |k,v|
+    v[:english] == emoticon
+  end
+  result[result.keys][:japanese]
 end
 
-def get_english_meaning
-  # code goes here
+# Method takes Japanese emoticon and returns name in English
+def get_english_meaning(library, emoticon)
+  hash <- load_library(library)
+  
 end
